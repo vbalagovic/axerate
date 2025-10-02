109d56c9c1ef917a32a6295420afb7b5582fde86
Vedran Balagović <vedran@qedcode.io>
2025-10-02 11:44:50 +0200

    yeah

--- strapi-backend/config/middlewares.ts
+++ strapi-backend/config/middlewares.ts
@@ -1,7 +1,45 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'cdn.ckeditor.com',
+            'strapi.io',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'cdn.ckeditor.com',
+          ],
+          'script-src': [
+            "'self'",
+            "'unsafe-inline'",
+            'cdn.ckeditor.com',
+          ],
+          'style-src': [
+            "'self'",
+            "'unsafe-inline'",
+            'cdn.ckeditor.com',
+          ],
+          'font-src': [
+            "'self'",
+            'cdn.ckeditor.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
